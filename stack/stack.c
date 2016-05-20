d8edd4e34392c195f58c61da4b3a21bfb3b611ab
Saterblue <Saterblue@users.noreply.github.com>
2016-05-20 20:35:26 +0800

    Update stack.c

--- stack/stack.c
+++ stack/stack.c
@@ -1,47 +1,38 @@
 #include "stack.h"
 
-#define NULL_POINT		100
-#define STACK_IS_FULL		101
-#define STACK_NOT_EMPTY 	102
-
-STACK* createStack(){
-	STACK *st = malloc(sizeof(STACK));
-	st->length = 0;
-	st->max = 128;
-	st->object = calloc( st->max, sizeof(void*));
-	return st;
+void InitStack(Stack* stack,int max) {
+	stack->length=0;
+	stack->maxLength = max;
+	stack->head = (int*)malloc(sizeof(int)*max);		
 }
-
-int deleteStack(STACK* del) {
-	if(del == NULL)
+	
+int push(Stack* stack,void* obj) {		
+	if( obj == null_ptr || obj == 0 || obj== NULL )
 		return NULL_POINT;
-	if( del->length != 0 ) 
-		return STACK_NOT_EMPTY;
-	free(del->object);
-	free(del);
-	del = NULL;
-	return 0;
-}
-
-int push(STACK* st,void* obj){
-	if(st == NULL) 
-		return NULL_POINT;
-	if(obj == NULL)
-		return NULL_POINT;
-	if(st->length == st->max)
+	if( stack->head == NULL )
+		return STACK_NOT_INIT;				
+	if( stack->length ==  stack->maxLength )
 		return STACK_IS_FULL;
-	*(st->object) = obj ;
-	st->object++;
-	st->length++; 
+	*(int*)((int) stack->head + sizeof(int)*stack->length) = (int)obj;
+	 stack->length++;
 	return 0;
 }
 
-void* pop(STACK* st) {
-	if(st == NULL)
+void* pop(Stack *stack) {
+	if( stack->head == NULL )
 		return NULL;
-	if(st->length == 0)
-		return NULL;		
-	st->object--;
-	st->length--;
-	return *(st->object);
+	if(  stack->length == 0 )
+		return NULL;
+	int address = (int)stack->head + sizeof(int)*(stack->length - 1);
+	int* point = (int*)*(int*)(address);
+	stack->length--;
+	return (void*)point;
+}
+
+int freeStack(Stack *stack) {
+	if( stack->head == NULL )
+		return 0;
+	free(stack->head);
+	stack->head=NULL;
+	return 0;
 }
