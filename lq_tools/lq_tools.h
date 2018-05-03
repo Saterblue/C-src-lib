21da9e0ab6ff70cd12bfbed2e1a6359da3011793
Saterblue <Saterblue@users.noreply.github.com>
2018-05-03 23:13:27 +0800

    Add files via upload

--- lq_tools/lq_tools.h
+++ lq_tools/lq_tools.h
@@ -22,8 +22,25 @@ example:
 output:
     2014-04-21 21:32:13
 */
-inline int lq_getLocaltime(char* buffer,size_t bufferSize);
+static inline int lq_getLocaltime(char* buffer,size_t bufferSize)
+{
+	time_t now = time(NULL);
+	struct tm* now_tm = localtime(&now);
+	char tmp[1024]  = {0};
+	sprintf(tmp,"%04d-%02d-%02d %02d:%02d:%02d",
+		now_tm->tm_year	+ 1900,
+		now_tm->tm_mon	+ 1,
+		now_tm->tm_mday,
+		now_tm->tm_hour,
+		now_tm->tm_min,
+		now_tm->tm_sec);
 
+	size_t len = strlen(tmp);
+	if(len + 1 > bufferSize){return len + 1;}
+	memcpy(buffer,tmp,len + 1);
+
+	return 0;
+}
 /*
 Parameters
 text	    -	pointer to the text change to lower
@@ -36,7 +53,14 @@ example:
 output:
     abcdefg
 */
-inline void lq_strToLower(char* text,size_t textLength);
+static inline void lq_strToLower(char* text,size_t textLength)
+{
+    int i = 0;
+    for(; i < textLength; i++)
+    {
+        text[i] = tolower(text[i]);
+    }
+}
 
 
 
